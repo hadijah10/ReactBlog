ad15a1814370a2a4930ebbe854d19dd94746945d
Evans Ofori <evansgyan7@gmail.com>
2023-11-10 20:52:32 +0000

    Add redux demo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,94 +1,65 @@
-import React from 'react'
-import { useState } from 'react'
-import { deleteDoc,doc ,db,collection,getDocs, auth} from '../firebase-config'
-import { useEffect } from 'react'
-import {useSelector} from 'react-redux';
-import toast from 'react-hot-toast';
-import { Card,Typography,Button, Space,Tooltip } from 'antd';
-import {DeleteOutlined,EditOutlined} from '@ant-design/icons'
-const {Title} = Typography
+import React from "react";
+import { useState } from "react";
+import {
+  deleteDoc,
+  doc,
+  db,
+  collection,
+  getDocs,
+  auth,
+} from "../firebase-config";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import toast from "react-hot-toast";
+import { Card, Typography, Button, Space, Tooltip } from "antd";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import { PostCard } from "../component/PostCard";
+import { fetchUsers, selectUsers } from "../redux/users";
+const { Title } = Typography;
 
 export default function Home() {
-    const userdata = useSelector((state) => state.auth.value)
-const [users,setUsers] = useState([{
-  title:'Tinitin a software di impaginazione come Aldus PageMake',
-  message:'Lorem ipsium',author:{name:'Hadija',email:'hakjd@gmail.com'}},
-{
-  title:'Tinitin a software di impaginazione come Aldus PageMake',
-  message:'Lorem ipsium',author:{name:'Hadija',email:'hakjd@gmail.com'}}]) 
-const usercollectionref = collection(db, "posts")
-const deleteUser = async(id)=> {
-  const user = doc(db,"posts",id)
- // 
-  await deleteDoc(user)
-}
-  useEffect(()=>{
-    const getusers = async()=> {
-      const data = await getDocs(usercollectionref)
-     setUsers(data.docs.map((doc) => ({...doc.data(),id:doc.id})))
-    }
-    getusers()
-  },[deleteUser])
+  const dispatch = useDispatch();
+  const { users, isLoading, error } = useSelector(selectUsers);
+  console.log("error", error)
+  console.log("users", users)
+  // const [users, setUsers] = useState([
+  //   {
+  //     title: "Tinitin a software di impaginazione come Aldus PageMake",
+  //     message: "Lorem ipsium",
+  //     author: { name: "Hadija", email: "hakjd@gmail.com" },
+  //   },
+  //   {
+  //     title: "Tinitin a software di impaginazione come Aldus PageMake",
+  //     message: "Lorem ipsium",
+  //     author: { name: "Hadija", email: "hakjd@gmail.com" },
+  //   },
+  // ]);
 
- 
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
   return (
     <>
       <Space
-      direction="vertical"
-      size="middle"     
+        direction="vertical"
+        size="middle"
         style={{
-          display: 'flex',
-          justifyContent:'start',
-          alignItems:'center',
-          minHeight:'90vh'
+          display: "flex",
+          justifyContent: "start",
+          alignItems: "center",
+          minHeight: "90vh",
         }}
       >
-        {users.map((user)=> {
-        return(
-        
-          <Card
-            key={user.id}
-            title={
-              <Space    direction="row" wrap size='small'
-              style={{
-                display: 'flex',
-                justifyContent:'space-between'
-              }}
-              >
-                  <Title level={3} wrap>{user.title}</Title>
-                  {userdata.isauth&&(auth?.currentUser.displayName == userdata?.name)&&(
-                     <Space direction='row' size='small' >
-                     <Tooltip title="Delete" >
-                       <Button type="primary" shape="circle" onClick={()=>deleteUser(user.id)} icon={ <DeleteOutlined />} />
-                     </Tooltip>
-                     <Tooltip title="Edit">
-                       <Button  type="primary" shape="circle"  icon={ <EditOutlined />} />
-                     </Tooltip>
-                   </Space>
-                  )}
-             
-              </Space>
-            }
-            direction="vertical"
-           
-            style={{
-              display: 'flex',
-              flexDirection:'column',
-              justifyContent:'center',
-              width: '90vw',
-
-            }}
-          >
-             <div>
-             <p>{user.message}</p>
-              <p>@{user?.author?.name}</p>
-             </div>
-          </Card>
-      
-        )
-        })}
-     </Space>
+        {isLoading ? (
+          <Typography>Loading...</Typography>
+        ) : (
+          users?.map((user) => {
+            return <PostCard user={user} key={user.id} />;
+          })
+        )}
+        {error && <Typography>An error occurred</Typography>}
+      </Space>
     </>
-
-  )
+  );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from'./auth' 
+import usersReducer from './users'
 
 export const store = configureStore({
   reducer: {
-    auth : authReducer
+    auth: authReducer,
+    users: usersReducer
   },
 })
